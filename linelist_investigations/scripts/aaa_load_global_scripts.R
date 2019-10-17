93c3707d55ab7af280689e5b5abc587fc1d2c331
Thibaut Jombart <thibautjombart@gmail.com>
2019-10-17 13:40:43 +0200

    adding gitignores

--- linelist_investigations/scripts/aaa_load_global_scripts.R
+++
@@ -1,6 +0,0 @@
-
-## read scripts
-path_to_global_scripts <- here::here("..", "..", "scripts")
-scripts_files <- dir(path_to_global_scripts,
-                     pattern = ".R$", full.names = TRUE)
-for (file in scripts_files) source(file, local = TRUE)
--- linelist_investigations/scripts/current_clean_data.R
+++ linelist_investigations/scripts/current_clean_data.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `aaa_clean_linelist`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2019-10-14 12:20:29
+## This file was last updated on the: 2019-10-17 13:40:10
 
 current_clean_data <- here::here("data",
                                  "clean",
-                                 "master_linelist_clean_2019-10-13.rds")
+                                 "master_linelist_clean_2019-10-16.rds")
