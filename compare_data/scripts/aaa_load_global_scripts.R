7234fb0c945c41dc4f9dfa117663c06cc23cd231
Thibaut Jombart <thibautjombart@gmail.com>
2019-10-28 16:22:45 +0200

    adding 'local = TRUE' to allcalls to 'source()'

--- compare_data/scripts/aaa_load_global_scripts.R
+++ compare_data/scripts/aaa_load_global_scripts.R
@@ -3,4 +3,4 @@
 path_to_global_scripts <- here::here("..", "global_scripts")
 scripts_files <- dir(path_to_global_scripts,
                      pattern = ".R$", full.names=TRUE)
-for (file in scripts_files) source(file)
+for (file in scripts_files) source(file, local = TRUE)
