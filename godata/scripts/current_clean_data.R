5886be2cf3fcf6d26b72871e2a286d3187e7b8b8
Thibaut Jombart <thibautjombart@gmail.com>
2019-11-01 08:45:21 +0200

    fixing data cleaning

--- godata/scripts/current_clean_data.R
+++ godata/scripts/current_clean_data.R
@@ -1,7 +1,7 @@
 ## This file is generated automatically by `aaa_clean_linelist`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2019-10-31 15:43:58
+## This file was last updated on the: 2019-11-01 08:41:12
 
 current_clean_cases <- here::here("data",
                                   "clean",
