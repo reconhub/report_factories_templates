e9463d9370b89ef9cab3a186d14f5c3d5af06893
Hollis <holliss@who.int>
2019-11-02 15:50:53 +0100

    Added in infrastructure for hierarchy joins

--- godata/scripts/current_data.R
+++ godata/scripts/current_data.R
@@ -4,11 +4,13 @@ raw_folder <- here::here("data", "raw")
 # Get the names of the most recent data files
 cases_file <- get_latest_data("cases", raw_folder)
 
-relationships_file <- get_latest_data("relationships", raw_folder)
+relationships_file <- get_latest_data("^relationships.*.xlsx", raw_folder)
 
-contacts_file <- get_latest_data("contacts", raw_folder)
+contacts_file <- get_latest_data("^contacts.*.xlsx", raw_folder)
 
-followups_file <- get_latest_data("followups", raw_folder)
+followups_file <- get_latest_data("^followups.*.xlsx", raw_folder)
+
+hierarchy_file <- get_latest_data("hierarchy.*.xlsx", raw_folder)
 
 
 
