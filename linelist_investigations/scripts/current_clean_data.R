a7a5ccbd4764f24182f1945372e2f8f1dc72a556
Thibaut Jombart <thibautjombart@gmail.com>
2019-11-06 12:58:22 +0000

    gitignoring current_clean_data.R as it will create regular conflicts

--- linelist_investigations/scripts/current_clean_data.R
+++
@@ -1,8 +0,0 @@
-## This file is generated automatically by `aaa_clean_linelist`
-## Do not edit it by hand!
-
-## This file was last updated on the: 2019-10-17 13:40:10
-
-current_clean_data <- here::here("data",
-                                 "clean",
-                                 "master_linelist_clean_2019-10-19.rds")
